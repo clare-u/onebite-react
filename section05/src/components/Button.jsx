1862c5d34b02853a878e9efea49106e2bae0e509
clare-u <clare.u.dev@gmail.com>
2024-06-10 14:47:26 +0900

    5.5) 이벤트 처리하기

--- section05/src/components/Button.jsx
+++ section05/src/components/Button.jsx
@@ -1,5 +1,15 @@
 const Button = ({text, color, children}) => {
-    return <button style={{ color: color}}>{text} - {color.toUpperCase()}{children}</button>;
+
+    // 이벤트 객체
+    const onClickButton = (e) => {
+        console.log(e)
+        console.log(text);
+    }
+
+    return <button 
+    onClick={onClickButton}
+    // onMouseEnter={onClickButton}
+    style={{ color: color}}>{text} - {color.toUpperCase()}{children}</button>;
 };
 
 Button.defaultProps = {
